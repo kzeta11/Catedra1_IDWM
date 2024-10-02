8a4c740ad8d52b1dea7671652c90c4fb4a17af79
kzeta11 <112991280+kzeta11@users.noreply.github.com>
2024-10-02 20:00:59 -0300

    feat: model de user

---
+++ catedra1/src/Models/User.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catedra1.src.Models
+{
+    public class User
+    {   
+       [Key]
+        public int Id { get; set; }
+        public required string Rut { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres.")]
+        public required string Nombre { get; set; }
+
+        [EmailAddress(ErrorMessage = "Correo electrónico no válido.")]
+        public required string Correo { get; set; }
+
+        [RegularExpression(@"masculino|femenino|otro|prefiero no decirlo", ErrorMessage = "El género debe ser masculino, femenino, otro o prefriero no decirlo.")]
+        public required string Genero { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+
+    }
+}
