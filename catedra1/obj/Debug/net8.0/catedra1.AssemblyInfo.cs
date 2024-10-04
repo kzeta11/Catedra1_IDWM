4603e0d05e3fa65d9238b38ad56e9ba2f1f0b603
kzeta11 <112991280+kzeta11@users.noreply.github.com>
2024-10-03 21:01:52 -0300

    feat: postman collection

--- catedra1/obj/Debug/net8.0/catedra1.AssemblyInfo.cs
+++ catedra1/obj/Debug/net8.0/catedra1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("catedra1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f197bd9299ff0220f553fc8a2fa22de825f971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e60d55bccb49c945ac1cc0f7205fbcacdd8cda")]
 [assembly: System.Reflection.AssemblyProductAttribute("catedra1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("catedra1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
